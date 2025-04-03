dfe5ef2e194101a0edf6f367fb3e49623a17a9aa
jonathan briceño <brimanz2014@gmail.com>
2025-04-03 13:35:08 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+
+#instalar pyaudio(pendiente)
+
+
+voice = sr.Recognizer()
+engine = pyttsx3.init()
+
+
+def talk():
+
+	mic = sr.Microphone()
+	with mic as source:
+
+		audio = voice.listen(source)
+
+	text = voice.recognize_google(audio, languaje="ES")
+
+	print(f'Dijiste: {text}')
+	return text.lower()
+
+if 'amazon' in talk():
+
+	engine.say('Que deseas comprar en Amazon')
+	engine.runAndWait()
+	text = talk()
+	webbrowser.open(f'https://www.amazon.es/s?k={text}')
